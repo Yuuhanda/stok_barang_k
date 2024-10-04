00d6acdeff09409953469a6ce1d94ab237ac3a15
YuuHanda <55708987+Yuuhanda@users.noreply.github.com>
2024-10-04 09:46:01 +0700

    alter version

--- backend/auth.php
+++ backend/auth.php
@@ -4,7 +4,10 @@ include('../database/config.php');
 
 $email = $_POST['email'];
 $password = $_POST['password'];
-
+$hashedPassword = substr($hashedPassword, 0, 60);
+//$hashedPassword = hash('sha256', $password);
+//$hashedPassword = substr($hashedPassword, 0, 60);
+//$query_admin = $mysqli->query("SELECT * FROM user WHERE email='$email' AND password='$hashedPassword'");
 $query_admin = $mysqli->query("SELECT * FROM user WHERE email='$email' AND password='$password'");
 $admin = $query_admin->num_rows;
 
